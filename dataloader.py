a359cdb6b7c81affc7bb3b260e855d569f8bdfd8
Geon-Gyu LEE <37532168+Geongyu@users.noreply.github.com>
2021-06-09 11:26:37 +0900

    Add files via upload

---
+++ dataloader.py
@@ -0,0 +1,128 @@
+
+import numpy as np
+import ipdb
+from torch._C import Value
+import torchvision
+import torchvision.transforms as transforms
+import torch
+from torch.utils.data import Dataset
+import ast
+import os
+import glob
+from PIL import Image
+import pandas as pd
+import random
+import torch.utils.data as data
+import torchvision.transforms.functional as TF
+import cv2
+
+class tox_21(Dataset) :
+    def __init__(self, file_root, mode="train", ratio=0.3, target="all") :
+        self.file_root = file_root
+        
+        try :      
+            self.data = pd.read_csv(self.file_root)
+        except :
+            raise ValueError("File root is not supported loacation")
+                    
+        mode = mode.lower()
+        
+        if self.mode not in ["train", "validation", "test", "t", "v"] :
+            raise ValueError("Not supported mode")
+        else :
+            self.mode = mode 
+        
+        if type(ratio) != int :
+            raise ValueError("The Ratio are must be INT type")
+        else : 
+            self.ratio = ratio 
+        
+        self.target = target  
+        self.check_target(target)
+            
+        self.trn_file, self.val_file, self.tst_file = self.split_data()
+    
+    def __len__(self) :
+        
+        if self.mode == "train" or self.mode == "t" :
+            return len(self.trn_file) 
+        elif self.mode == "validation" or self.mode=="v" :
+            return len(self.val_file)
+        elif self.mode == "test" :
+            return len(self.tst_file) 
+        else :
+            return len(self.file_root)
+        
+    def check_target(self, target) :
+        targets = ["NR-AR", "NR-AR-LBD", "NR-AhR", "NR-Aromatase", "NR-ER",
+                "NR-ER-LBD", "NR-PPAR-gamma", "SR-ARE", "SR-ATAD5", "SR-HSE", 
+                "SR-MMP", "SR-p53", "all"]
+        
+        targets = [i.lower() for i in targets] 
+        tar = target.lower()
+
+        if tar not in targets :
+            raise ValueError("Not Support Target!") 
+        
+        return 0 
+    
+    def split_data(self) :
+        ratio = self.ratio 
+        
+        tst_ratio = 1-ratio 
+        tst_idx = int(len(self.data) * tst_ratio)
+        tst_file = self.data[tst_idx:]
+        tst_file = tst_file.reset_index(drop=True)
+        
+        tmp_file = self.data[:tst_idx]
+        tmp_file = tmp_file.reset_index(drop=True)
+        
+        tmp = len(tmp_file)
+        tmp_ratio = 0.8
+        tmp_idx = int(tmp * tmp_ratio) 
+        
+        trn_file = tmp_file[:tmp_idx]
+        trn_file = trn_file.reset_index(drop=True)
+        
+        val_file = tmp_file[tmp_idx:]
+        val_file = val_file.reset_index(drop=True)
+        
+        return trn_file, val_file, tst_file 
+    
+    def get_x_y(self, idx) :
+        
+        if self.mode == "train" :
+            molecular = self.trn_file['smiles'].loc[idx]
+            labels = list(self.trn_file.drop(['smiles'], axis=1).loc[idx].values)
+            
+        elif self.mode == "validation" :
+            molecular = self.val_file['smiles'].loc[idx]
+            labels = list(self.val_file.drop(['smiles'], axis=1).loc[idx].values)
+            
+        else : #self.mode == "test" :
+            molecular = self.tst_file['smiles'].loc[idx]
+            labels = list(self.tst_file.drop(['smiles'], axis=1).loc[idx].values)
+        
+        return molecular, labels
+    
+    def __getitem__(self, idx):
+        
+        molecular, labels = self.get_x_y(idx)
+        
+        target_label = self.target.lower()
+        
+        targets = ["NR-AR", "NR-AR-LBD", "NR-AhR", "NR-Aromatase", "NR-ER",
+                "NR-ER-LBD", "NR-PPAR-gamma", "SR-ARE", "SR-ATAD5", "SR-HSE", 
+                "SR-MMP", "SR-p53"]
+        
+        targets = [i.lower() for i in targets] 
+        
+        if target_label != "all" :
+            index = int(targets.index(target_label)) 
+            labels = labels[index]
+        
+        return molecular, labels 
+
+if __name__=='__main__':
+
+    import ipdb; ipdb.set_trace()
---
+++ utils.py
@@ -0,0 +1,237 @@
+import os
+import glob
+
+import numpy as np
+from skimage import io
+
+try:
+    import torch
+except:
+    pass
+
+import shutil
+from collections import Iterable
+import matplotlib.pyplot as plt
+from slacker import Slacker
+import argparse
+from model import *
+from torch.utils.data.sampler import SubsetRandomSampler
+from losses import DiceLoss,tversky_loss, NLL_OHEM
+from optimizers import RAdam
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+import torch.utils.data as data
+
+class AverageMeter(object):
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.sum_2 = 0 # sum of squares
+        self.count = 0
+        self.std = 0
+
+    def update(self, val=None, n=1):
+        if val!=None: # update if val is not None
+            self.val = val
+            self.sum += val * n
+            self.sum_2 += val**2 * n
+            self.count += n
+            self.avg = self.sum / self.count
+            self.std = np.sqrt(self.sum_2/self.count - self.avg**2)
+        else:
+            pass
+
+class Logger(object):
+    def __init__(self, path, int_form=':03d', float_form=':.4f'):
+        self.path = path
+        self.int_form = int_form
+        self.float_form = float_form
+        self.width = 0
+
+    def __len__(self):
+        try: return len(self.read())
+        except: return 0
+
+    def write(self, values):
+        if not isinstance(values, Iterable):
+            values = [values]
+        if self.width == 0:
+            self.width = len(values)
+        assert self.width == len(values), 'Inconsistent number of items.'
+        line = ''
+        for v in values:
+            if isinstance(v, int):
+                line += '{{{}}} '.format(self.int_form).format(v)
+            elif isinstance(v, float):
+                line += '{{{}}} '.format(self.float_form).format(v)
+            elif isinstance(v, str):
+                line += '{} '.format(v)
+            else:
+                raise Exception('Not supported type.',v)
+        with open(self.path, 'a') as f:
+            f.write(line[:-1] + '\n')
+
+    def read(self):
+        with open(self.path, 'r') as f:
+            log = []
+            for line in f:
+                values = []
+                for v in line.split(' '):
+                    try:
+                        v = float(v)
+                    except:
+                        pass
+                    values.append(v)
+                log.append(values)
+        return log
+
+def save_checkpoint(state, is_best, work_dir, filename='checkpoint.pth'):
+    checkpoint_path = os.path.join(work_dir, filename)
+    if is_best:
+        torch.save(state, checkpoint_path)
+        shutil.copyfile(checkpoint_path,
+                        os.path.join(work_dir, 'model_best.pth'))
+
+def load_exam(exam_dir, ftype='png'):
+
+    file_extension = '.'.join(['*', ftype])
+    data_paths = glob.glob(os.path.join(exam_dir, file_extension))
+    data_paths = sorted(data_paths, key=lambda x: x.split('/')[-1]) # sort by filename
+
+    slices = []
+    for data_path in data_paths:
+        arr = io.imread(data_path)
+        slices.append(arr)
+
+    data_3d = np.stack(slices)
+
+    return data_3d
+
+def pad_3d(data_3d, target_length, padding_value=0):
+
+    d, h, w = data_3d.shape # assume single channel
+    margin = target_length - d # assume that h and w are sufficiently larger than target_length
+    padding_size = margin // 2
+    upper_padding_size = padding_size
+    lower_padding_size = margin - upper_padding_size
+
+    padded = np.pad(data_3d, ((upper_padding_size, lower_padding_size),
+                              (0,0), (0,0)),
+                    'constant', constant_values=(padding_value,padding_value))
+
+    return padded, (upper_padding_size, lower_padding_size)
+
+def calc_stats(data_root):
+    
+    data_ids = os.listdir(os.path.join(data_root, 'images'))
+
+    mean_meter = AverageMeter()
+    std_meter = AverageMeter()
+
+    for data_id in data_ids:
+        image_dir = os.path.join(data_root, 'images', data_id)
+        image_3d = load_exam(image_dir, ftype='png')
+        pixel_mean = image_3d.mean()
+        pixel_std = image_3d.std()
+
+        mean_meter.update(pixel_mean, image_3d.size)
+        std_meter.update(pixel_std, image_3d.size)
+
+    total_mean = mean_meter.avg
+    total_std = np.sqrt(std_meter.sum_2/std_meter.count)
+
+    return {'mean': total_mean, 'std': total_std}
+
+def select_arch(arch_name, in_shape=(1,256,256), padding_size=1, momentum=0.1) :
+    if arch_name == "unet" :
+        net = Unet2D(in_shape=in_shape, padding=padding_size, momentum=momentum)
+    elif arch_name == 'mtlunet' :
+        net = MTL_Unet(in_shape=in_shape, padding=padding_size, momentum=momentum)
+    elif arch_name == 'naive' :
+        net = MTL_NAIVE_UNET(in_shape=in_shape, padding=padding_size, momentum=momentum)
+    elif arch_name == 'supervision_unet' :
+        net = Supervision_Unet(in_shape=in_shape, padding=padding_size, momentum=momentum)
+    else :
+        raise ValueError('{} is Not supported yet.'.format(arch_name))
+    return net
+
+def select_optimizer(optim_function, initial_lr, net, weight_decay=0.0001, momentum=0.9) :
+    if optim_function == 'sgd':
+        optimizer = torch.optim.SGD(net.parameters(), lr=initial_lr, momentum=momentum,
+                                weight_decay=weight_decay)
+    elif optim_function == 'adam' :
+        optimizer = torch.optim.Adam(net.parameters(), lr=initial_lr, weight_decay=weight_decay)
+    elif optim_function == 'radam':
+        optimizer = RAdam(net.parameters(), lr=initial_lr, weight_decay =weight_decay)
+    else:
+        raise ValueError('{} loss is not supported yet.'.format(optim_function))
+    return optimizer
+
+def check_correct_forgget(output, target, ephoch,
+size, correct, before_correct) :
+    if ephoch == 0 :
+        correct = ((output-target) == 0).float() # 맞추면 1 틀리면 0
+        forget = None
+        added = None
+    else :
+        added = ((output-target) == 0).float()
+        forget = np.zeros(size) - (((before_correct - added) == 1).float())
+        correct += added
+    return correct, forget, added
+
+def send_slack_message(token,channel,messge):
+    token = token
+    slack = Slacker(token)
+    slack.chat.post_message(channel, messge)
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+class Performance(object) :
+    def __init__(self) :
+        self.confusionmatrix = [0, 0, 0, 0]
+        # TP, TN, FP, FN
+
+    def cal_confusion(self, pred, real) :
+        if real == 1 and pred == 1 :
+            self.confusionmatrix[0] += 1
+        elif real == 1 and pred == 0 :
+            self.confusionmatrix[3] += 1
+        elif real == 0 and pred == 0 :
+            self.confusionmatrix[1] += 1
+        else :
+            self.confusionmatrix[2] += 1
+
+    def return_matrix(self) :
+        return self.confusionmatrix
+
+if __name__ == "__main__" :
+    import os
+    work_dir = "/daintlab/home/geongyu/MTL/MTL/adam/Self_Supervision/add_factor/Inpainting_half_training_less_decay1"
+
+    trn_logger = Logger(os.path.join(work_dir, 'train.log'))
+    trn_raw_logger = Logger(os.path.join(work_dir, 'train_raw.log'))
+    val_logger = Logger(os.path.join(work_dir, 'validation.log'))
+    tst_logger = Logger(os.path.join(work_dir, 'test.log'))
+
+    draw_self_supervision_curve(work_dir, trn_logger, val_logger, tst_logger)
+
+    ''' work_dir = "/daintlab/home/geongyu/MTL/MTL/Self_Supervision/Inpainting_half_training2"
+    trn_logger = Logger(os.path.join(work_dir, 'train.log'))
+    trn_raw_logger = Logger(os.path.join(work_dir, 'train_raw.log'))
+    val_logger = Logger(os.path.join(work_dir, 'validation.log'))
+    draw_self_supervision_curve(work_dir, trn_logger, val_logger)
+    '''
